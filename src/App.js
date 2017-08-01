14e4b3592aa4f2aa286b0dc5768b5e64ce30b499
matthova <matthova87@gmail.com>
2017-08-01 10:40:56 -0700

    New format with react-router. Added prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import './App.css';
-import Station from './Station';
-import stations from './stations.json';
+import "./App.css";
+import Station from "./Station";
+import stations from "./stations.json";
 
 class App extends Component {
   constructor() {
     super();
 
-    this.stations = stations;
+    this.stations = stations.root.stations.station;
 
     this.state = {
-      position: null,
+      position: null
     };
   }
 
   setLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(position => {
       this.setState({ position });
     });
   }
@@ -26,20 +27,46 @@ class App extends Component {
   }
 
   renderStationList() {
-    return this.stations.root.stations.station.map((station, i) => {
-      return <Station key={i} station={station} />
+    return this.stations.map((station, i) => {
+      return (
+        <Link key={i} className="station-name" to={station.abbr}>
+          {station.name}
+        </Link>
+      );
     });
   }
 
   renderSelectedStation() {
-    return <div>Cool</div>;
+    return (
+      <Switch>
+        <Route
+          path="/:station"
+          render={({ match }) => {
+            const station = this.stations.find(station => {
+              return station.abbr === match.params.station;
+            });
+
+            return station ? <Station station={station} /> : null;
+          }}
+        />
+      </Switch>
+    );
   }
 
   render() {
     return (
-      <div className="App container">
-        {this.renderStationList()}
-      </div>
+      <Router>
+        <div className="App container">
+          <div className="stations">
+            <nav>
+              {this.renderStationList()}
+            </nav>
+          </div>
+          <div className="selected-station">
+            {this.renderSelectedStation()}
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/Station.js
+++ src/Station.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import request from 'request-promise';
-import propTypes from 'prop-types';
-import autobind from 'autobind';
+import React, { Component } from "react";
+import request from "request-promise";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import Destination from './Destination';
+import Destination from "./Destination";
 
 export default class Station extends Component {
   constructor() {
@@ -11,45 +11,42 @@ export default class Station extends Component {
 
     this.state = {
       etds: [],
-      updater: null,
-    }
-
-    autobind(this);
+      updater: null
+    };
   }
 
   renderDestinations() {
+    return null;
     try {
       const destinations = this.props.station.root.station[0].etd;
       return destinations.map((destination, i) => {
-        return <Destination key={i} destination={destination} />
+        return <Destination key={i} destination={destination} />;
       });
     } catch (ex) {
       // In case error when parsing trains, return null
-      console.error('Parsing train error', ex);
+      console.error("Parsing train error", ex);
       return null;
     }
   }
 
-    // Collect ETD info from a single station and return it as a JSON object
+  // Collect ETD info from a single station and return it as a JSON object
   async getStationETDs(station) {
     const stationUrl = `https://api.bart.gov/api/etd.aspx?cmd=etd&orig=${station}&key=MW9S-E7SL-26DU-VV8V&json=y`;
     try {
-      const reply = await request(stationUrl, { json: true })
+      const reply = await request(stationUrl, { json: true });
       reply.station = station;
       return reply;
-    } catch(ex) {
+    } catch (ex) {
       return null;
     }
-  }
-
-  handleClick() {
-    console.log('eyyy');
   }
 
   render() {
     return (
       <div className="station">
-        <h1 className="station-name" onClick={this.handleClick}>{this.props.station.name}</h1>
+        <h1>
+          {this.props.station.name}
+        </h1>
         {this.renderDestinations()}
       </div>
     );
@@ -58,4 +55,4 @@ export default class Station extends Component {
 
 Station.propTypes = {
   station: propTypes.object.isRequired
-}
+};
